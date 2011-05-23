f911c0287c09357c6a48321af98babdcaf4b4435
Prashant Shah <pshah.mumbai@gmail.com>
2011-05-23 17:25:25 +0530

    Udpated latex script
    Signed-off-by: Prashant Shah <pshah.mumbai@gmail.com>

--- latex/latex_test.sh
+++ latex/latex_test.sh
@@ -52,8 +52,8 @@ echo \\newpage >> $CURDIR/TEX
 echo \\vspace*{3cm}>>$CURDIR/TEX
 
 echo Scilab numbering policy used in this document and the relation to the above book.>>$CURDIR/TEX
-echo \\begin{description}>>$CURDIR/TEX >> $CURDIR/TEX
-echo \\item[Exa]  Example \(Solved example\)>>$CURDIR/TEX >>$CURDIR/TEX
+echo \\begin{description}>>$CURDIR/TEX 
+echo \\item[Exa]  Example \(Solved example\)>>$CURDIR/TEX 
 echo \\item[Eqn]  Equation \(Particular equation of the above book\)>>$CURDIR/TEX
 echo \\item[AP]   Appendix to Example\(Scilab Code that is an Appednix to a particular Example of the above book\)>>$CURDIR/TEX
 echo \\end{description}>>$CURDIR/TEX
@@ -69,13 +69,15 @@ echo \\listofcode >>$CURDIR/TEX
 echo>> $CURDIR/TEX
 j=0;
 k=1;
-sort -t '#' -k 3 -k 1 -g  $Data_all > database_sort
+#sort -t '#' -k 3,3 -k 1,1 -g $Data_all > database_sort
+sort -t '.' -k 1,1n -k 2,2n $Data_all > database_sort
+
 while IFS=# read col1 col2 col3 col4 col5 col6 col7 col8 col9; do
 
 chap_diff=$(($col1 - $j))
 if [ $chap_diff -eq 1 ]; then
-	echo \\chapter{$col2}>>TEX
-	echo >>TEX
+	echo \\chapter{$col2}>>$CURDIR/TEX
+	echo >>$CURDIR/TEX
 fi
 
 if [ $chap_diff -gt 1 ]; then
@@ -99,12 +101,14 @@ then
  echo \\tcaption{$col4}{$col4} >> $CURDIR/TEX
  echo \\lstinputlisting{../$col6}  >> $CURDIR/TEX
  echo \\end{code} >> $CURDIR/TEX
+ echo >>$CURDIR/TEX
 fi
 
 if [ $col7 =  D ]
 then
-echo check Appendix \\ref{AP:$col5} for dependency \$$col5\$ >> $CURDIR/TEX
-echo \\\\ >>$CURDIR/TEX
+#echo check Appendix \\ref{AP:$col9} for dependency \$$col5\$ >> $CURDIR/TEX
+echo check Appendix \\ref{AP:$col9} for dependency \{\\begin\{alltt\} $col5 \\end\{alltt\}\} >> $CURDIR/TEX
+echo  >> $CURDIR/TEX
 fi
 
 
@@ -116,11 +120,11 @@ fi
 if [ $col7 =  R  ]
 then 
 echo $col6 > $CURDIR/Figure_files
-	if  grep -c ".jpg\|.JPEG\|.jpeg\|.png" $CURDIR/Figure_files 
+	if  grep -c ".jpg\|.JPEG\|.png\|.jpeg" $CURDIR/Figure_files 
 	then
 		 echo \\curlable{Fig~$col3} >> $CURDIR/TEX
 		 echo \\begin{figure} >> $CURDIR/TEX
-		 echo \\includegraphics{../$col6}  >> $CURDIR/TEX
+		 echo \\includegraphics[width=6in]{../$col6}  >> $CURDIR/TEX
 		 echo \\caption{$col4} >> $CURDIR/TEX
 		 echo \\end{figure} >> $CURDIR/TEX
 		 echo >> $CURDIR/TEX
@@ -131,19 +135,26 @@ fi
 j=$col1
 done < database_sort
 rm Figure_files
+
+
+if [  -s $Dep_dat ]
+then
+
 i=1;
 echo \\chapter*{Appendix} >>$CURDIR/TEX
 
-while IFS=# read col1 col2 col3 col4; do 
+while IFS=# read col1 col2 col3 col4; do
 echo \\curlable{AP~$i} >> $CURDIR/TEX;
 echo \\begin{code} >> $CURDIR/TEX;
-echo \\label{AP:$col1} >> $CURDIR/TEX
-echo \\tcaption{\$$col1\$}{\$$col1\$} >> $CURDIR/TEX
+echo \\label{AP:$col4} >> $CURDIR/TEX
+echo \\tcaption {$col3}{$col3} >> $CURDIR/TEX
 echo \\lstinputlisting{../$col2}  >> $CURDIR/TEX
 echo \\end{code} >> $CURDIR/TEX
 echo >> $CURDIR/TEX
 let "i+=1"
 done < $Dep_dat
+
+fi
 
 cat Initial_body  TEX  > TEX_final.tex
 echo \\end{document} >> $CURDIR/TEX_final.tex
--- latex/pdf_creator.sh
+++ latex/pdf_creator.sh
@@ -2,6 +2,8 @@ $ cat changedir.sh
 #!/bin/bash
 
 ./latex_test.sh $1 $2 $3 $4
-pdflatex TEX_final.tex 
+pdflatex TEX_final.tex
+pdflatex TEX_final.tex
+pdflatex TEX_final.tex
 pdflatex TEX_final.tex
 #rm log.txt
