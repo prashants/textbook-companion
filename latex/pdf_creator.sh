a169d7314ab09487d2c6843f5f58a921a9383bb6
Prashant P. Shah <pshah.mumbai@gmail.com>
2011-06-23 12:20:10 +0530

    Udpated bash script
    Signed-off-by: Prashant P. Shah <pshah.mumbai@gmail.com>

--- latex/pdf_creator.sh
+++ latex/pdf_creator.sh
@@ -4,10 +4,7 @@ $ cat changedir.sh
 
 if [ -f TEX_final.pdf ]
 then
-	rm TEX_final.*
-	rm database_sort
-	rm database_sort.csv
-        rm TEX
+	rm TEX_final.pdf
 fi
 
 ./latex_test.sh $1 $2 $3 $4
@@ -15,5 +12,8 @@ pdflatex TEX_final.tex > log.txt
 pdflatex TEX_final.tex >> log1.txt
 pdflatex TEX_final.tex >> log1.txt
 pdflatex TEX_final.tex >> log1.txt
-
+rm TEX_final.*
+rm database_sort
+rm database_sort.csv
+rm TEX
 rm log1.txt
